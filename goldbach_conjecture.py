ba056ca2b4f35b31991c38099a498e5be870ab30
workspace <projects@MacBook-Pro.home>
2021-09-19 17:39:55 -0400

    new logic working adding to current saving in case i break something

--- goldbach_conjecture.py
+++ goldbach_conjecture.py
@@ -1,84 +1,64 @@
 import math
 
-def isEven (i):
-	if i%2==0:
-	    return True
-	    print("helloworld")
 
-	else:
-		return False
-		print("helloworldd")
+def isEven(i):
+    if i % 2 == 0:
+        return True
+        print("helloworld")
+
+    else:
+        return False
+        print("helloworldd")
 
 
 def isPrime(i):
-     from math import sqrt
-     if i == 0 or i == 1:
-         flag = False
-         print("if start")
-     elif i == 2:
-         flag = True  
-     else:
-         for i in range(2, i):
-             if i % i == 0:
-                 flag = False
-                 break
-             else:
-                 flag = True
-                 return flag
+    from math import sqrt
+    if i == 0 or i == 1:
+        flag = False
+        print("if start")
+    elif i == 2:
+        flag = True
+    else:
+        for i in range(2, i):
+            if i % i == 0:
+                flag = False
+                break
+            else:
+                flag = True
+                return flag
 
-def isnum( string ):
+
+def isnum(string):
     if string.isdigit():
-       return True
+        return True
     else:
         return False
 
 
+def test():
+    print("test works")
 
 
-
+i_list = []
 def main():
 
-    print("helloworld")
+    test()
 
+    for i in range(1, 11):
 
-for i in xrange(1,10):
-    
-    if i%2==0:
-        
-        print(i)
-        
-            i_list = []
+        if i % 2 == 0:
+            num = i
+            print(num)
+            for i in range(1, num):
+                j = num - 1
+                print(j)
+                print(i)
 
-            for i in range(1,Uinput):
-                    
-                    j = Uinput - i #Break down into two numbers
-                
-                if isPrime(i) and isPrime(j):
-                    
-                    i_list.append(i) #Record displayed numbers
-                    
-                    if j in i_list:
-                            
-                            pass
-                    
-                    else:
-                            
-                            print( '{0} = {1} + {2}'.format(Uinput, i, j))
-        
+            i_list.append(i)
+
         else:
-            
-            print('Input error!')
+            print("helloworldd")
 
-       
-    else:
-        print("helloworldd")
-    
 
-    
-	
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
-
-
-
-
