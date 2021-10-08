c5235930b3d7fbcbf342f119eff23026bd362f32
workspace <projects@MacBook-Pro.home>
2021-10-08 13:44:16 -0400

    working on using user input eith turtle

--- goldbach_conjecture.py
+++ goldbach_conjecture.py
@@ -1,14 +1,16 @@
 import math
+import turtle
+from turtle import *
 
 
 def isEven(i):
     if i % 2 == 0:
         return True
-        print("helloworld")
+        # print("helloworld")
 
     else:
         return False
-        print("helloworldd")
+        # print("helloworldd")
 
 
 def isPrime(num):
@@ -37,44 +39,50 @@ def isnum(string):
 
 
 def main():
-
+    i_list = []
     for i in range(5, 50):
 
         if isEven(i):
             num = i
-            i_list = []
-            for i in range(2, num):
+            print(num)
+
+            for i in range(1, num):
                 j = num - i
                 # print(j)
                 # print("num")
                 # print(num)
 
                 if isPrime(i) and isPrime(j):
-                    print(j)
+                    # print(j)
                     i_list.append(i)
-                    print("num")
-                    print(num)
+                    # print("num")
+                    # print(num)
                     print("answer")
                     print(i, j)
-                    # print(i_list)
                     flag = True
                     break
 
                     if j in i_list:
-                        # print(i_list)
                         pass
                     else:
                         # print("1else")
                         #print(i, j)
                         pass
                 else:
-                    #print(i, j)
-                    # print("2else")
+                    #print("not prime")
                     pass
 
         else:
-            print("not Even")
+            # print(i)
+            #print("not Even")
+            pass
 
+
+# taking input
+number = turtle.textinput("user input", "enter a even number")
+
+# print number input
+print(number)
 
 if __name__ == "__main__":
     main()
